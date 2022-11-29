faceb60b7f284f5c339a601e4ae75b6a5114f9a2
Abiramirajasekar <abiramirajasekar002@gmail.com>
2022-11-30 00:17:04 +0530

    Project Update

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -3,6 +3,8 @@ import Project1 from "./Project-1/Project1";
 import Project2 from "./Project-2/Project2";
 import Project3 from "./Project-3/Project3";
 import Project4 from "./Project-4/Project4";
+import Project5 from "./Project-5/Project5";
+import Project6 from "./Project-6/Project6";
 import style from "./project.module.css";
 
 function Project() {
@@ -20,6 +22,8 @@ function Project() {
           <Project2 />
           <Project4 />
           <Project3 />
+          <Project5 />
+          <Project6/>
         </div>
       </div>
     </>
